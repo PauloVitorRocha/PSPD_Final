62810f4b201f15c1e31f2bd42419c415eab5f287
PauloVitorRocha <paulovitorrocha.unb@gmail.com>
2022-05-01 21:50:52 -0300

    primeira parte com 2 hosts

--- primeira_parte/SS.py
+++ primeira_parte/SS.py
@@ -1,16 +1,18 @@
 import sys
-
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode
 from pyspark.sql.functions import split
 
+
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: app.py <hostname> <port>", file=sys.stderr)
+    if len(sys.argv) != 5:
+        print("Usage: app.py <hostname> <port> <hostname> <port>", file=sys.stderr)
         sys.exit(-1)
 
     host = sys.argv[1]
     port = int(sys.argv[2])
+    host2 = sys.argv[3]
+    port2 = int(sys.argv[4])
 
     #inicia a sessão no spark
     spark = SparkSession\
@@ -26,6 +28,15 @@ if __name__ == "__main__":
         .option('port', port)\
         .load()
 
+    lines2 = spark\
+      .readStream\
+      .format('socket')\
+      .option('host', host2)\
+      .option('port', port2)\
+      .load()
+
+    lines = lines.union(lines2)
+
     # separa as linhas em palavras
     words = lines.select(
         # explode transforma cada item do array em uma linha
@@ -37,12 +48,13 @@ if __name__ == "__main__":
     # agrupa as palavras todas na mesma tabela
     wordCounts = words.groupBy('word').count()
 
-    # Start running the query that prints the running counts to the console
-    # escreve a resposta em modo de 
+    # escreve a resposta toda vez que houver mudança na stream
     query = wordCounts\
         .writeStream\
         .outputMode('complete')\
+        .option("numRows", 50)\
         .format('console')\
         .start()
 
     query.awaitTermination()
+    
